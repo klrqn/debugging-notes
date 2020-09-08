e354772056188d3720af500216fb2bdcc4ed75f5
john doe <scottbruceburton@gmail.com>
2020-09-08 13:43:24 -0400

    better loop

--- factorialLog.py
+++ factorialLog.py
@@ -10,9 +10,7 @@ logging.debug('Start of Program')
 def factorial(n):
     logging.debug('start of factorial(%s%%)' % (n))
     total = 1
-    for i in range(n + 1):
-        if i == 0:
-            continue
+    for i in range(1, n + 1):
         total *= i
         logging.debug('i is ' + str(i) + ', total is ' + str(total))
     logging.debug('End of factorial(%s%%)' % (n))
