17460d9ab56a17d8254cdb623f5d8b5d6e992658
john doe <scottbruceburton@gmail.com>
2020-09-12 12:31:36 -0400

    added logging

--- debug_cointoss.py
+++ debug_cointoss.py
@@ -1,15 +1,24 @@
 #! python3
 
+import logging
 import random
+
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+
 guess = ''
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
+    logging.info(f'guess should be {guess}')
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
+logging.info(f'toss should be {toss}')
+logging.debug(f'{toss} == {guess}')
 if toss == 1:
     toss = 'heads'
+    logging.debug(f'{toss} == {guess}')
 else:
     toss = 'tails'
+    logging.debug(f'{toss} == {guess}')
 if toss == guess:
     print('You got it!')
 else:
